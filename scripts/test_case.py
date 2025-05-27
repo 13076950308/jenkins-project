ed54b341598170620115ca0e23ba78883e94f576
13076950308 <1611714537@qq.com>
2025-05-27 21:09:45 +0800

    Initial commit

--- scripts/test_case.py
+++ scripts/test_case.py
@@ -1,7 +1,9 @@
 class TestCase:
     def test_001(self):
-        assert 1
+        assert 0
     def test_002(self):
         assert 1
     def test_003(self):
         assert 1
+    def test_004(self):
+        assert False
